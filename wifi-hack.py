2d5ec2376683cc16707f4dd477b7afdb235c1d84
jesperVB-dev <jesper.vanbondt@icloud.com>
2017-09-11 01:11:57 +0200

    Added possibility for using and testing passwords
    Added possibility for using and testing passwords. It first changes an xml document that is placeholder, then it imports the new xml document so that it can check if it can connect to it. this is nesescery for windows!

--- wifi-hack.py
+++ wifi-hack.py
@@ -1,4 +1,16 @@
 from subprocess import check_output
+import os
+
+xmlfile = open("D:\Wi-Fi-telenet-6101F.xml")
+
+with open('D:\Wi-Fi-telenet-6101F.xml', 'r') as input_file, open('D:\Wi-Fi-telenet-6101Fnew.xml', 'w') as output_file:
+    for line in input_file:
+        if 'keyMaterial' in line:
+            output_file.write('				<keyMaterial>Heyhoegaathetmetjou</keyMaterial>\n')
+        else:
+            output_file.write(line)
+
+os.popen('netsh wlan add profile filename="D:\Wi-Fi-telenet-6101Fnew.xml"')
 output = check_output('netsh wlan connect SBHG-adm', shell=True)
 
 print output
